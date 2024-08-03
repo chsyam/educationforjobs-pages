eb1e4557757f75c8b7feefe49b337681b05861dc
Syam Kumar Chintakayala <syamkumar6845@gmail.com>
2024-08-04 03:46:56 +0530

    updated the api to fetch all blogs

--- pages/api/blogs/getAllBlogs.jsx
+++ pages/api/blogs/getAllBlogs.jsx
@@ -2,8 +2,7 @@ import axios from "axios";
 
 export async function getAllBlogs() {
     try {
-        const apiUrl = "http://localhost:8080/api/blogs/all";
-        // "https://educationforjobs.onrender.com/api/blogs/all";
+        const apiUrl = "https://educationforjobs.onrender.com/api/blogs/all";
         const response = await axios.get(apiUrl);
         return response.data;
     } catch (error) {
